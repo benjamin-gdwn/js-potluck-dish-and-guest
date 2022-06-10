d6f4022977d7a4de661f40d4a3deee689eb3bd96
Ben_G <ben.goodwin.work1@gmail.com>
2022-06-10 10:08:57 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,82 @@
+// invite button
+const addGuestButton = document.querySelector(".invite");
+// label for the invite button
+const guestInputLabel = document.querySelector(".add-guest label");
+// text input box
+const guestInput = document.querySelector(".add-guest input");
+// unordered list (not yet visible)
+const guestList = document.querySelector(".guest-list");
+// span class for number of guests attending
+const guestCount = document.querySelector(".attendance");
+// alert when guest list is full (not yet visible)
+const guestFull = document.querySelector(".alert");
+
+const assignButton = document.querySelector(".assign");
+
+const assignedItems = document.querySelector(".assigned-items");
+
+addGuestButton.addEventListener("click", function () {
+  const guest = guestInput.value;
+  // console.log(guest);
+  if (guest !== " ") {
+    addToList(guest);
+    updateGuestCount();
+  }
+});
+
+const clearInput = function () {
+  guestInput.value = "";
+};
+
+const addToList = function (guest) {
+  const listItem = document.createElement("li");
+  listItem.innerText = guest;
+  guestList.append(listItem);
+  clearInput();
+};
+
+const updateGuestCount = function () {
+  let guests = document.querySelectorAll(".guest-list li");
+  guestCount.innerText = guests.length;
+  if (guests.length === 8) {
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    guestInputLabel.classList.add("hide");
+    guestFull.classList.remove("hide");
+  }
+};
+
+const assignItems = function () {
+  const potLuckItems = [
+    "spring rolls",
+    "courgette noodles",
+    "sandwiches",
+    "cous-cous salad",
+    "gouda and red pepper salad",
+    "white gazpacho",
+    "peach and prosciutto bruschetta",
+    "peanut butter energy bars",
+    "cream cheese pie with summer fruit",
+    "no bake nutella cakes",
+    "muffin tin ice cream cakes",
+    "veggie nori rolls"
+  ];
+
+  const allGuests = document.querySelectorAll(".guest-list li");
+
+  for (let guest of allGuests) {
+    let randomPotLuckIndex = Math.floor(Math.random() * potLuckItems.length);
+    let randomPotLuckItem = potLuckItems[randomPotLuckIndex];
+
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotLuckItem}.<br>`;
+    assignedItems.append(listItem);
+
+    potLuckItems.splice(randomPotLuckIndex, 1);
+  }
+};
+
+assignButton.addEventListener("click", function () {
+  assignItems();
+  assignButton.disabled = true;
+});
